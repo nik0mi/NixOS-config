03d2a7288b961110db373b6b04b62dd4a02b457a
nik0mi <dertonov@gmail.com>
2025-10-27 17:13:41 +0700

    finally can start 6.py without shell

--- modules/home-manager/common/helix/languages.nix
+++ modules/home-manager/common/helix/languages.nix
@@ -1,6 +1,28 @@
 { pkgs, ... }:
 
 {
+
+  programs.helix.languages = {
+    language-server = {
+      pylsp = {
+        command = "${pkgs.python3Packages.python-lsp-server}/bin/pylsp";
+      };
+    };
+    language = [
+      {
+        name = "python";
+        auto-format = true;
+        formatter = {
+          command = "${pkgs.black}/bin/black";
+          args = [
+            "--quiet"
+            "-"
+          ];
+        };
+        language-servers = [ "pylsp" ];
+      }
+    ];
+  };
   home.packages = with pkgs; [
     #JSON
     vscode-json-languageserver
@@ -20,10 +42,17 @@
     nixd
 
     #PYTHON
-    ty
-    ruff
-    jedit
+    # ty
+    # ruff
+    black
+    # jedit
     python313Packages.python-lsp-server
-    python313Packages.jedi
+    (python3.withPackages (
+      ps: with ps; [
+        tkinter
+      ]
+    )) # python313Packages.jedi
+    # python313Packages.tkinter
+    # python313Packages.mathutils
   ];
 }
